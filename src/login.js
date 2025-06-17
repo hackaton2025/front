b944f88372941b9f510938f68c15e6e9d1032243
stec <m.stec0112@gmail.com@gmail.com>
2025-06-17 16:45:30 +0200

    Added login

---
+++ src/login.js
@@ -0,0 +1,35 @@
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let submitButton = document.getElementById('submit');
+let loginResult = document.getElementById('loginResult');
+let spinner = document.getElementById('spinner');   
+
+submitButton.addEventListener('click', async function(event) {
+    event.preventDefault();
+
+    if (username.value === '' || password.value === '') {
+        loginResult.innerText = 'All fields are required!';
+        return;
+    }
+
+
+    let userData = {
+        username: username.value,
+        password: password.value
+    };
+
+    spinner.style.display = 'flex';
+
+    let response  = await fetch(`http://192.168.109.120:3000/login?username=${username.value}&password=${password.value}`);
+    let data = await response.json();
+    console.log(data);
+
+    if (data.success == true) {
+        console.log(data)
+        document.cookie = `token=${data.sessionToken}; path=/`
+        window.location.href = 'index.html';
+    } else {
+        spinner.style.display = 'none';
+        loginResult.innerText = 'Logowanie nie powiodło się: ' + data.message;
+    }
+});
